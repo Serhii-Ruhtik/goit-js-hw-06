aceaeda0cf67d7de9d9c3c05e7f0d6fa8343718e
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2022-10-04 10:44:44 +0300

    test-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,38 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.map(function (element) {
+  const elements = document.createElement("li");
+  elements.textContent = element;
+  elements.classList.add("item");
+  console.log(elements);
+
+  const ulIngredientsRef = document.querySelector('#ingredients');
+ulIngredientsRef.append(elements);
+})
+
+
+
+
+
+
+// const ingredientsRef = document.createElement('li');
+// ingredientsRef.classList.add('item');
+// ingredientsRef.textContent = 'pfujkjdjr'
+
+// console.log(ingredientsRef);
+
+// const ingredientsLinkRef = document.createElement('a');
+// ingredientsLinkRef.classList.add('list__ingredients');
+// ingredientsLinkRef.textContent = 'my room';
+// ingredientsLinkRef.href = '/profile';
+
+// console.log(ingredientsLinkRef);
+
+// ingredientsRef.appendChild(ingredientsLinkRef);
+// console.log(ingredientsRef)
+
+
+// const ulIngredientsRef = document.querySelector('#ingredients');
+// ulIngredientsRef.appendChild(ingredientsRef)
